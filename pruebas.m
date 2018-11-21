d1aa28c7e02733d8cddcd44a73382f75ee1e993b
tomasferrerb <tomas.ferrer.briceno@gmail.com>
2018-11-21 00:29:16 -0300

    sockets

--- pruebas.m
+++ pruebas.m
@@ -6,6 +6,9 @@ imgR= imread(img);
 [row,col,prof] = size(imgR);
 vector= img2vector(imgR);
 packet = vector2packet(vector, Npaquetes);
+
+%%%AQUI SE ENVIAN 
+
 vector2 = packet2vector(packet);
 b = vector2img(vector2,row,col);
 imshow(uint8(b));
