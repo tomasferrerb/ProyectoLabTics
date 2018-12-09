c6aacd58a925d34677346b067bd241aafdc72b03
tomasferrerb <tomas.ferrer.briceno@gmail.com>
2018-12-09 00:01:56 -0300

    C actualizado

--- C.m
+++ C.m
@@ -12,21 +12,21 @@ fopen(udpC);
 
 %Recibir paquetes y ordenar en matriz
 count=1;
+total_datos=0;
 while 1
     vec=fread(udpC,250,'uint8');
-    if count==1
-        N=vec(1);
-        row= vec(3);
-        col=vec(4);
-    end
-    matriz(vec(2),:)=vec;
-    count=count+1;
-    if count==N
-        break
+    if vec(1)==0:%PRIMER PAQUETE
+        row=vec(2);
+        col=vec(3);
+        N=vec(4);
+    elseif ~(vec(1)== N):        
+        matriz(vec(1),:)=vec;
+        count=count+1;
+        total_datos=total_datos+vec(2);
     end
 end
 
-[vector row col] = packet2vector(matriz);
+vector  = packet2vector(matriz);
 image = vector2img(vector, row, col);
 
 imshow(uint8(image)); 
